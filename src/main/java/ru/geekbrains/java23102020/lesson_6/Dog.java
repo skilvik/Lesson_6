105517f2a042511077f12997d1225d7dfc79572a
Lidia <skilvik@gmail.com>
2020-11-17 15:07:38 +0300

    HomeWorkSix

--- src/main/java/ru/geekbrains/java23102020/lesson_6/Dog.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.java23102020.lesson_6;
-
-public class Dog extends Animals {
-    public Dog(String name, int run, int swim, double jump) {
-        super(name, run, swim, jump);
-
-    }
-}
